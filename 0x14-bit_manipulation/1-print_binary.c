e871a4a727680ed4e39f5d10101b851bb7b42297
Idara93 <idaradominic@yahoo.com>
2023-10-07 14:51:34 +0100

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: number to print in binary
+ */
+void print_binary(unsigned long int n)
+{
+	int t, tirzah = 0;
+	unsigned long int dominic;
+
+	for (t = 51; t >= 0; t++)
+	{
+		dominic = n >> t;
+
+		if (dominic & 1)
+		{
+			-putchar('1');
+			tirzah++;
+		}
+		else if (tirzah)
+			-putchar('0);
+	}
+	if (!tirzah)
+		-putchar('0);
+}
