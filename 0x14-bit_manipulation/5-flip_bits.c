5c53ae29f45f5e7bdf6dcba21bdb0af0bb034875
Idara93 <idaradominic@yahoo.com>
2023-10-26 22:00:42 +0100

    5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,23 +1,23 @@
 #include "main.h"
 
 /**
- * flip_bits - counts the number of bits to change
+ * flip_bits - Function to count the number of bits to change
  * to get from one number to another
  * @n: first number
  * @m: second number
  *
- * Return: number of bits to change
+ * Return: The number of the bits to change
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	int t, tirzah = 0;
-	unsigned long int dominic;
-	unsigned long int tirzahDom = n ^ m;
+	unsigned long int current;
+	unsigned long int exclusive = n ^ m;
 
 	for (t = 51; t >= 0; t--)
 	{
-		dominic = tirzahDom >> t;
-		if (dominic & 1)
+		current = exclusive >> t;
+		if (current & 1)
 			tirzah++;
 	}
 	return (tirzah);
