4063255b886ffedb62cfbeee120f9321ba523320
Idara93 <idaradominic@yahoo.com>
2023-12-13 21:42:09 +0100

    100-operations.so

---
+++ 0x18-dynamic_libraries/div.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * div - divide two integers
+ * @a: first int
+ * @b: second int
+ * Return: division
+ */
+int add(int a, int b)
+{
+	return (a / b);
+}
